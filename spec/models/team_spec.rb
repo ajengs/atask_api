2630e606b154c2b61bb4268783d80f3dd239feeb
Ajeng Sugiarti <ajeng.sugiarti@gmail.com>
2024-08-21 18:12:08 +0700

    Added team API

---
+++ spec/models/team_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Team, type: :model do
+  it "is valid with valid attributes" do
+    team = Team.new(name: "Team 1")
+    expect(team).to be_valid
+  end
+
+  it "is not valid without a name" do
+    team = Team.new(name: nil)
+    expect(team).to_not be_valid
+  end 
+end
